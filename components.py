32312facfd7c0502eec339dcea9ac1c8aebd3b29
hinichijou <163578861+hinichijou@users.noreply.github.com>
2025-09-20 17:42:00 +0300

    Generalized the return link component a bit further

--- components.py
+++ components.py
@@ -6,5 +6,8 @@ from localization import get_localization
 # A test for creating and sharing some basic components between templates. Could build a library of basic components and call them with functions from the templates
 # Other methods that Jinja seems to offer: template inheritance, include statement, macros
 
-def get_return_to_index_button():
-        return Markup(f"<a href='/'> {get_localization(LocalizationKeys.return_to_index_button)} </a>")
+def link_with_text(t):
+    return Markup(f"<a href='/'> {t} </a>")
+
+def return_to_index_link():
+        return link_with_text(get_localization(LocalizationKeys.return_to_index_button))

93fcab82373d0234c124592b19c5004857a71686
hinichijou <163578861+hinichijou@users.noreply.github.com>
2025-09-27 13:41:09 +0300

    Some refactoring to component parameters

--- components.py
+++ components.py
@@ -24,91 +24,90 @@ def kwargsToParamString(**kwargs):
 def submit_button_with_text(t):
     return Markup(f"<input type='submit' value='{t}' />")
 
-def user_input(type, name, required, *args, **kwargs):
+def user_input(type, name, *args, **kwargs):
     return Markup(f"<input \
                         type='{type}' \
                         name='{name}' \
-                        required='{required}' \
                         {argsToParamString(*args)} \
                         {kwargsToParamString(**kwargs)} \
                     />")
 
-def user_input_with_header(headerkey, type, name, required, *args, **kwargs):
-    return Markup(f"{get_localization(headerkey)} <br />{user_input(type, name, required, *args, **kwargs)}")
+def user_input_with_header(headerkey, type, name, *args, **kwargs):
+    return Markup(f"{get_localization(headerkey)} <br />{user_input(type, name, *args, **kwargs)}")
 
-def select(name, required, options, **kwargs):
+def select(name, options, *args, **kwargs):
     return Markup(f"<select  \
                         name='{name}' \
-                        required='{required}' \
+                        {argsToParamString(*args)} \
                         {kwargsToParamString(**kwargs)} \
                     > \
                         {options} \
                     </select>")
 
-def select_with_header(headerkey, name, required, options, **kwargs):
-    return Markup(f"{get_localization(headerkey)} <br />{select(name, required, options, **kwargs)}")
+def select_with_header(headerkey, name, options, *args, **kwargs):
+    return Markup(f"{get_localization(headerkey)} <br />{select(name, options, *args, **kwargs)}")
 
-def text_input(headerkey, type, name, required, minlength, maxlength, **kwargs):
-    return user_input_with_header(headerkey, \
-                                    type, \
-                                    name, \
-                                    required, \
-                                    minlength=minlength, \
-                                    maxlength=maxlength, \
+def text_input(headerkey, type, name, minlength, maxlength, *args, **kwargs):
+    return user_input_with_header(headerkey,
+                                    type,
+                                    name,
+                                    *args,
+                                    minlength=minlength,
+                                    maxlength=maxlength,
                                     **kwargs
                                 )
 
-def number_input(headerkey, name, min, max, required, **kwargs):
+def number_input(headerkey, name, min, max, *args, **kwargs):
      oninput = """oninput="this.value = Math.min('""" + str(max) + "', Math.max(this.value|0, '" + str(min) + """'))\""""
 
-     return user_input_with_header(headerkey, \
-                                    "number", \
-                                    name, \
-                                    required, \
-                                    oninput, \
-                                    min=min, \
-                                    max=max, \
+     return user_input_with_header(headerkey,
+                                    "number",
+                                    name,
+                                    oninput,
+                                    *args,
+                                    min=min,
+                                    max=max,
                                     **kwargs
                                 )
 
-def date_input(headerkey, name, required, **kwargs):
-     return user_input_with_header(headerkey, \
-                                    "datetime-local", \
-                                    name, \
-                                    required, \
+def date_input(headerkey, name, *args, **kwargs):
+     return user_input_with_header(headerkey,
+                                    "datetime-local",
+                                    name,
+                                    *args,
                                     **kwargs
                                 )
 
 def inert_select(headerkey, name, selection):
     options = f"<option value='{ selection[0] },{ selection[1] }'>{ get_localization(selection[1]) }</option>"
 
-    return select_with_header(headerkey, name, True, options, inert = True)
+    return select_with_header(headerkey, name, options, "required", "inert")
 
 def hidden_input(n, v):
-    return Markup(f"<input type='hidden' name='{n}' value='{v}' />")
+    return user_input('hidden', n, value=v)
 
-def inert_text_input(n, v):
-    return Markup(f"<input inert=True type='text' name='{n}' value='{v}' />")
+def inert_text_input(headerkey, name, value):
+    return user_input_with_header(headerkey, "text", name, "inert", value=value)
 
 def link_with_text(l, t):
     return Markup(f"<a href='{l}'> {t} </a>")
 
 def account_name_input():
-     return text_input(LocalizationKeys.account_name_header, \
-                        "text", \
-                        "username", \
-                        True, \
-                        constants.username_minlength, \
-                        constants.username_maxlength
+     return text_input(LocalizationKeys.account_name_header,
+                        "text",
+                        "username",
+                        constants.username_minlength,
+                        constants.username_maxlength,
+                        "required"
                     )
 
 def base_password_input(headerkey, name):
-     return text_input(headerkey, \
-                        "password", \
-                        name, \
-                        True, \
-                        constants.password_minlength, \
-                        constants.password_maxlength
+     return text_input(headerkey,
+                        "password",
+                        name,
+                        constants.password_minlength,
+                        constants.password_maxlength,
+                        "required"
                     )
 
 def password_input():
@@ -118,64 +117,64 @@ def repeat_password_input():
      return base_password_input(LocalizationKeys.repeat_password_header, "repeat_password")
 
 def course_name_input():
-     return text_input(LocalizationKeys.course_name_header, \
-                        "text", \
-                        "coursename", \
-                        True, \
-                        constants.coursename_minlength, \
-                        constants.coursename_maxlength
+     return text_input(LocalizationKeys.course_name_header,
+                        "text",
+                        "coursename",
+                        constants.coursename_minlength,
+                        constants.coursename_maxlength,
+                        "required"
                     )
 
 def amount_players_input():
-     return number_input(LocalizationKeys.amount_players_header, \
-                            "num_players", \
-                            constants.round_min_players, \
-                            constants.round_max_players, \
-                            True,
+     return number_input(LocalizationKeys.amount_players_header,
+                            "num_players",
+                            constants.round_min_players,
+                            constants.round_max_players,
+                            "required",
                             value = constants.round_min_players
                         )
 
 def course_holes_input():
-     return number_input(LocalizationKeys.course_holes_header, \
-                            "num_holes", \
-                            constants.course_holes_min, \
-                            constants.course_holes_max, \
-                            True,
+     return number_input(LocalizationKeys.course_holes_header,
+                            "num_holes",
+                            constants.course_holes_min,
+                            constants.course_holes_max,
+                            "required",
                             value = constants.course_holes_default
                         )
 
 def start_date_input(value):
-     return date_input(LocalizationKeys.start_time_header, "start_time", True, value=value)
+     return date_input(LocalizationKeys.start_time_header, "start_time", "required", value=value)
 
 
-def course_select(courses, required):
+def course_select(courses, *args):
     options = f"<option hidden selected value=''>{ get_localization(LocalizationKeys.course_select_default) }</option>"
 
     for course in courses:
         options += f"<option value='{ course["id"] }'>{ course["coursename"] }</option>"
 
-    return select_with_header(LocalizationKeys.select_course_header, "course_select", required, options)
+    return select_with_header(LocalizationKeys.select_course_header, "course_select", options, *args)
 
-def select_from_option_tuple(name, headerkey, defaultkey, required, selection):
+def select_from_option_tuple(name, headerkey, defaultkey, selection, *args):
     options = f"<option hidden selected value=''>{ get_localization(defaultkey) }</option>"
 
     for s in selection:
         options += f"<option value='{ s[0] },{ s[1] }'>{ get_localization(s[1]) }</option>"
 
-    return select_with_header(headerkey, name, required, options)
+    return select_with_header(headerkey, name, options, *args)
 
-def course_type_select(ctypes, required):
-    return select_from_option_tuple("type_select", LocalizationKeys.course_type_header, LocalizationKeys.course_type_default, required, ctypes)
+def course_type_select(ctypes, *args):
+    return select_from_option_tuple("type_select", LocalizationKeys.course_type_header, LocalizationKeys.course_type_default, ctypes, *args)
 
-def course_difficulty_select(cds, required):
-    return select_from_option_tuple("difficulty_select", LocalizationKeys.course_difficulty_header, LocalizationKeys.course_diff_default, required, cds)
+def course_difficulty_select(cds, *args):
+    return select_from_option_tuple("difficulty_select", LocalizationKeys.course_difficulty_header, LocalizationKeys.course_diff_default, cds, *args)
 
 def par_input(index):
      return number_input(LocalizationKeys.par_header, \
                             f"par_{index}", \
                             constants.hole_par_min, \
                             constants.hole_par_max, \
-                            True,
+                            "required",
                             value = constants.hole_par_default
                         )
 
@@ -184,7 +183,7 @@ def length_input(index):
                             f"length_{index}", \
                             constants.hole_length_min, \
                             constants.hole_length_max, \
-                            True,
+                            "required",
                             value = constants.hole_length_default
                         )
 
